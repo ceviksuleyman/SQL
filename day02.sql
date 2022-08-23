43dc9c4108d2d91be5b2bdce31ce7083c77d111b
cevik <suleymancevik64@gmail.com>
2022-08-23 23:20:43 +0300

    22-08-23

--- day02.sql
+++ day02.sql
@@ -165,3 +165,40 @@ ise_baslama DATE,
 
 
 
+INSERT INTO calisanlar VALUES('10002', 'Mehmet Yılmaz' ,12000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10008', null, 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10010', Mehmet Yılmaz, 5000, '2018-04-14'); --unique
+INSERT INTO calisanlar VALUES('10004', 'Veli Han', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10005', 'Mustafa Ali', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10006', 'Canan Yaş', NULL, '2019-04-12');-- constraint Not NULL
+INSERT INTO calisanlar VALUES('10003', 'CAN', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14'); -- Can ikidefa kabul etmedi unique
+INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14'); -- Not Null
+INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14');
+INSERT INTO calisanlar VALUES('', 'osman can', 2000, '2018-04-14'); -- hiclik 2 defa kabul etmedi unique primary key
+INSERT INTO calisanlar VALUES( '10002', 'ayse Yılmaz' ,12000, '2018-04-14'); -- primary key
+INSERT INTO calisanlar VALUES( null, 'filiz ' ,12000, '2018-04-14'); -- primary key null kabul etmez.
+
+
+select * from calisanlar;
+
+--Foreign Key
+Create Table adresler(
+adres_id char(5),
+	sokak varchar(20),
+	cadde varchar(30),
+	sehir varchar(20),
+	CONSTRAINT id_pk foreign key (adres_id) references calisanlar(id)
+);
+
+INSERT INTO adresler VALUES('10003','Mutlu Sok', '40.Cad.','IST');
+INSERT INTO adresler VALUES('10003','Can Sok', '50.Cad.','Ankara');
+INSERT INTO adresler VALUES('10002','Ağa Sok', '30.Cad.','Antep');
+
+select * from adresler;
+
+
+
+
+
+
