a43fabbe08bb7cf8a65e21f47d0ed1ef48b48d60
cevik <suleymancevik64@gmail.com>
2022-08-27 17:58:37 +0300

    22-07-27

--- day06/02_Limit.sql
+++ day06/02_Limit.sql
@@ -10,18 +10,18 @@ CREATE TABLE kisiler (
 	adres VARCHAR(50) 
 );
     
-INSERT INTO kisiler VALUES(1, 123456789, 'Ali','Can', 3000,'Istanbul');
-INSERT INTO kisiler VALUES(2, 234567890, 'Veli','Cem', 2890,'Ankara');
-INSERT INTO kisiler VALUES(3, 345678901, 'Mine','Bulut',4200,'Adiyaman');
-INSERT INTO kisiler VALUES(4, 256789012, 'Mahmut','Bulut',3150,'Adana');
-INSERT INTO kisiler VALUES (5, 344678901, 'Mine','Yasa', 5000,'Ankara');
-INSERT INTO kisiler VALUES (6, 345458901, 'Veli','Yilmaz',7000,'Istanbul');
+INSERT INTO kisiler VALUES(1,123456789, 'Ali','Can', 3000,'Istanbul');
+INSERT INTO kisiler VALUES(2,234567890, 'Veli','Cem', 2890,'Ankara');
+INSERT INTO kisiler VALUES(3,345678901, 'Mine','Bulut',4200,'Adiyaman');
+INSERT INTO kisiler VALUES(4,256789012, 'Mahmut','Bulut',3150,'Adana');
+INSERT INTO kisiler VALUES(5,344678901, 'Mine','Yasa', 5000,'Ankara');
+INSERT INTO kisiler VALUES(6,345458901, 'Veli','Yilmaz',7000,'Istanbul');
 INSERT INTO kisiler VALUES(7,113456789, 'Ali','Can', 3000,'Istanbul');
 INSERT INTO kisiler VALUES(8,224567890, 'Veli','Cem', 2890,'Ankara');
 INSERT INTO kisiler VALUES(9,335678901, 'Mine','Bulut',4200,'Ankara');
 INSERT INTO kisiler VALUES(10,446789012, 'Mahmut','Bulut',3150,'Istanbul');
-INSERT INTO kisiler VALUES (11,554678901, 'Mine','Yasa', 5000,'Ankara');
-INSERT INTO kisiler VALUES (12,665458901, 'Veli','Yilmaz',7000,'Istanbul');
+INSERT INTO kisiler VALUES(11,554678901, 'Mine','Yasa', 5000,'Ankara');
+INSERT INTO kisiler VALUES(12,665458901, 'Veli','Yilmaz',7000,'Istanbul');
 
 Select * From kisiler;
 
