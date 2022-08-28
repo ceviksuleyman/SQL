c584f1027e62815023f7e47a70f8230df3007d16
cevik <suleymancevik64@gmail.com>
2022-08-29 00:26:04 +0300

    22-08-29

--- day03/03_Select_Between_IN.sql
+++ day03/03_Select_Between_IN.sql
@@ -1,9 +1,7 @@
 /*
   SELECT komutunda  BETWEEN kosulu
   
- * Between belirttigimiz 2 veri arasidaki bilgileri listeler
- 
- * Between de belirttigimiz degerlerde listelemeye dahildir.
+  Between belirttigimiz 2 veri arasidaki bilgileri listeler. Between de belirttigimiz degerlerde listelemeye dahildir.
 */
 create table personel(
 	id char(4),
@@ -21,36 +19,44 @@ insert into personel values('1006', 'Sena Beyaz', 100000);
  AND (ve) : Belirtilen sartlarin her ikisi de gerceklesiyorsa o kayit listelenir.Bir tanesi gerceklesmezse listelemez.
  Select * From matematik sinav1 > 50 AND sinav2 > 50, hem sinav1 hem de sinav2 alani 50 den buyuk olan kayitlari listeler.
  
+ 
  OR (veya) : Belirtilen sartlardan biri gerceklesirse, kayit listelenir.
  Select * From matematik sinav1 > 50 OR sinav2 > 50, Hem sinav1 veya sinav2 alani, 50 den buyuk olan kayitlari listeler.
+ 
+ 
+ IN : Birden fazla mantiksal ifade ile tanimlayabilecegimiz durumlari tek komutta yazabilme imkani verir. Farkli sutunlar icin IN KULLANILAMAZ
 */
 
 select * from personel;
 
 --TASK-1 : id'si 1003 ile 1005 arasinda olan personel bilgisini listeleyin.
-select * from personel Where id Between '1003' and '1005';
+
+Select * From personel Where id Between '1003' and '1005';
 -- 2.Yontem
 Select * From personel Where id >= '1003' and id <= '1005';
 
 
--- TASK : Derya Soylu ile Yavuz Bal arasindaki personel bilgisini listeleyiniz.
+
+--TASK-2 : Derya Soylu ile Yavuz Bal arasindaki personel bilgisini listeleyiniz.
+
 Select * From personel Where isim Between 'Derya Soylu' And 'Yavuz Bal';
 
 
--- TASK : Maasi 70000 ve ismi Sena olan personeli listele
+
+--TASK-3 : Maasi 70000 ve ismi Sena olan personeli listele
+
 Select * From personel Where maas = 70000 OR isim = 'Sena Beyaz'; -- iki sarti birden saglayan veri olmadigi icin And'de bos gelir
 
------------------*************************************------------------------********************-
-/*
-   IN : Birden fazla mantiksal ifade ile tanimlayabilecegimiz durumlari tek komutta yazabilme imkani verir.
-   !! Farkli sutunlar icin IN KULLANILAMAZ
-*/
 
--- TASK : id'si 1001, 1002 ve 1004 olan personelin bilgilerini listele
+
+--TASK-4 : id'si 1001, 1002 ve 1004 olan personelin bilgilerini listele
+
 Select * From personel Where id = '1001' or id = '1002' or id = '1004';
 -- 2.Yontem
 Select * From personel Where id IN('1001','1002','1004');
 
 
--- TASK : maasi sadece 70000, 100000 olan personeli listele
+
+--TASK-5 : maasi sadece 70000, 100000 olan personeli listele
+
 Select * From personel Where maas in('70000','100000');
