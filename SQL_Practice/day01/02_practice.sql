0459d21ab652aaec2fd91f029d4e31016d47b109
cevik <suleymancevik64@gmail.com>
2022-08-29 22:16:34 +0300

    22-08-29

---
+++ SQL_Practice/day01/02_practice.sql
@@ -0,0 +1,20 @@
+CREATE TABLE workers(
+  id int,
+  name varchar(20),
+  title varchar(60),
+  manager_id int
+);
+INSERT INTO workers VALUES(1, 'Ali Can', 'Dev', 2);
+INSERT INTO workers VALUES(2, 'John Davis', 'QA', 3);
+INSERT INTO workers VALUES(3, 'Angie Star', 'Dev Lead', 4);
+INSERT INTO workers VALUES(4, 'Amy Sky', 'CEO', 5);
+
+Select * From workers;
+
+--TASK-1 : Tabloya company_industry isminde sütun ekleyiniz
+Alter Table workers
+Add Column company_industry varchar(30);
+
+--TASK-2 : TABLOYA worker_address sütunu ve defaullt olarakta 'Miami, FL, USA' adresini ekleyiniz.
+Alter Table workers
+Add Column worker_address varchar(30) default 'Miami, FL, USA';
